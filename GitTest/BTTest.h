dc9d0c8822bc0fcca0d0fbf6a7d667a02d91798f
bthornsberry1 <brett.thornsberry@gmail.com>
2014-03-23 14:32:15 -0500

    added test class

---
+++ GitTest/BTTest.h
@@ -0,0 +1,13 @@
+//
+//  BTTest.h
+//  GitTest
+//
+//  Created by Brett Thornsberry on 3/23/14.
+//  Copyright (c) 2014 Brett Thornsberry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTTest : NSObject
+
+@end
---
+++ GitTest/BTTest.m
@@ -0,0 +1,13 @@
+//
+//  BTTest.m
+//  GitTest
+//
+//  Created by Brett Thornsberry on 3/23/14.
+//  Copyright (c) 2014 Brett Thornsberry. All rights reserved.
+//
+
+#import "BTTest.h"
+
+@implementation BTTest
+
+@end
